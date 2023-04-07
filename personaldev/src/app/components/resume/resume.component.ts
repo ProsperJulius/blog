2c4774615087a0abd3df92f86cfac2b86f6bc4e4
prospermavunga <julius.mavunga@ei-technologies.com>
2023-04-08 01:21:51 +0200

    clean home page and added a new pic and resume

--- personaldev/src/app/components/resume/resume.component.ts
+++ personaldev/src/app/components/resume/resume.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resume.component.css']
 })
 export class ResumeComponent implements OnInit {
-  pdfSource = "assets/ResumeProsperJuliusMavunga.pdf";
+  pdfSource = "assets/ProsperJuliusFullStackDev.pdf";
 
   constructor() { }
 
