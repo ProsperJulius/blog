99408afc632012af22717bd863456c3bc61eb9ff
Mavunga Prosper Julius <prmavhunga@gmail.com>
2024-03-01 13:45:39 +0000

    Adds a capital city quiz

--- src/feature/portfolio/quiz-capital/capital-city-opitions.tsx
+++ src/feature/portfolio/quiz-capital/capital-city-opitions.tsx
@@ -1,4 +1,4 @@
-vimport toast, { Toaster } from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 import { useAppSelector } from "../../../app/hooks";
 import { useEffect, useState } from "react";
 import classNames from "classnames";
@@ -7,33 +7,28 @@ export const CapitalCityOptions = () => {
   const answer = useAppSelector((state) => state.capitalCityQuiz.answer);
   const options = useAppSelector((state) => state.capitalCityQuiz.options);
   const [disableSelection, setDisableSelection] = useState<boolean>(false);
-  const [userAnswer, setUserAnswer] = useState<string>("");
+  const [userAnswer,setUserAnswer] = useState<string>("");
 
   useEffect(() => {
     setDisableSelection(false);
-    setUserAnswer("");
   }, [answer?.capital]);
 
   const capitalCitiesOptions = options.map((country) => {
     return (
       <li
-        className={classNames({
-          clicked: country.capital === userAnswer,
-          btn: true,
-          correct:userAnswer.length && country.capital === answer?.capital,
-          wrong: userAnswer.length && country.capital === userAnswer && country.capital !== answer?.capital,
-        })}
+      className={classNames({clicked:country.capital === userAnswer,btn:true})}
         key={country.iso3}
         onClick={() => {
           if (!disableSelection) {
             setDisableSelection(true);
-            setUserAnswer(country.capital);
+            setUserAnswer(country.capital)
             country.capital === answer?.capital
               ? toast.success("Your answer is correct 😊")
               : toast.error(`The correct answer is ${answer?.capital} 😜`);
           }
         }}
       >
+       
         {country?.capital}
       </li>
     );
