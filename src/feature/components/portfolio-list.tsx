185a43484d5829c8fbc6986b28ca95f48f7faa15
Mavunga Prosper Julius <prmavhunga@gmail.com>
2024-03-01 13:45:39 +0000

    Adds a capital city quiz

--- src/feature/components/portfolio-list.tsx
+++ src/feature/components/portfolio-list.tsx
@@ -3,41 +3,65 @@ import { NavLink } from "react-router-dom";
 export const PortfolioList = () => {
   return (
     <div className="mt-5 portfolio">
-      <div className="normal-text portfolio-element">
-        <p className="project-list-color fs-4">
-          Todo list application click{" "}
-          <NavLink to="todo" className="remove-decoration project-list-color">
-            here
-          </NavLink>{" "}
-          to open the app
-        </p>
+      <ul>
+        <li>
+          <div className="normal-text portfolio-element">
+            <p className="project-list-color fs-4">Todo list application</p>
 
-        <p className="text-strong">
-          {" "}
-          <span className="text-strong">Tech Stack:</span> React, Typescript and
-          Redux
-        </p>
-      </div>
+            <NavLink
+              to="todo"
+              className="remove-decoration project-list-color btn btn-primary mt-2 mb-3"
+            >
+              Lunch
+            </NavLink>
+            <p className="text-strong">
+              {" "}
+              <span className="text-strong">Tech Stack:</span> React, Typescript
+              and Redux
+            </p>
+          </div>
+        </li>
 
-      <div className="normal-text portfolio-element">
-        <p className="project-list-color fs-4">
-          Paint app click{" "}
-          <NavLink to="paint" className="remove-decoration project-list-color">
-            here
-          </NavLink>{" "}
-          to open the app
-        </p>
+        <li>
+          <div className="normal-text portfolio-element">
+            <p className="project-list-color fs-4">Paint app</p>
+            <NavLink
+              to="paint"
+              className="remove-decoration project-list-color btn btn-primary mt-2 mb-3"
+            >
+              Lunch
+            </NavLink>
 
-        <p className="text-strong">
-          {" "}
-          <span className="text-strong">Tech Stack:</span> React, Typescript and
-          Redux
-        </p>
-      </div>
+            <p className="text-strong">
+              {" "}
+              <span className="text-strong">Tech Stack:</span> React, Typescript
+              and Redux
+            </p>
+          </div>
+        </li>
 
-      <div className="normal-text portfolio-element">
-        <p className="project-list-color fs-4">More to come : )</p>
-      </div>
+        <li>
+          <div className="normal-text portfolio-element">
+            <p className="project-list-color fs-4">Capital cities quiz</p>
+            <NavLink
+              to="capitalcityquiz"
+              className="remove-decoration project-list-color btn btn-primary mt-2 mb-3"
+            >
+              Lunch
+            </NavLink>
+            <p className="text-strong">
+              <span className="text-strong">Tech Stack:</span> React, Typescript
+              and Redux
+            </p>
+          </div>
+        </li>
+
+        <li>
+          <div className="normal-text portfolio-element">
+            <p className="project-list-color fs-4">More to come : )</p>
+          </div>
+        </li>
+      </ul>
     </div>
   );
 };
---
+++ src/feature/portfolio/quiz-capital/capital-city-opitions.tsx
@@ -0,0 +1,43 @@
+import toast, { Toaster } from "react-hot-toast";
+import { useAppSelector } from "../../../app/hooks";
+import { useEffect, useState } from "react";
+import classNames from "classnames";
+
+export const CapitalCityOptions = () => {
+  const answer = useAppSelector((state) => state.capitalCityQuiz.answer);
+  const options = useAppSelector((state) => state.capitalCityQuiz.options);
+  const [disableSelection, setDisableSelection] = useState<boolean>(false);
+  const [userAnswer,setUserAnswer] = useState<string>("");
+
+  useEffect(() => {
+    setDisableSelection(false);
+  }, [answer?.capital]);
+
+  const capitalCitiesOptions = options.map((country) => {
+    return (
+      <li
+      className={classNames({clicked:country.capital === userAnswer,btn:true})}
+        key={country.iso3}
+        onClick={() => {
+          if (!disableSelection) {
+            setDisableSelection(true);
+            setUserAnswer(country.capital)
+            country.capital === answer?.capital
+              ? toast.success("Your answer is correct 😊")
+              : toast.error(`The correct answer is ${answer?.capital} 😜`);
+          }
+        }}
+      >
+       
+        {country?.capital}
+      </li>
+    );
+  });
+
+  return (
+    <div>
+      <Toaster />
+      <ul>{capitalCitiesOptions}</ul>
+    </div>
+  );
+};
