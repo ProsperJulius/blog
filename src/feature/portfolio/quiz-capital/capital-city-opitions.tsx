ba149a60f7f0ded7ade8c7aa8fd6b1c2dd88ca7b
Mavunga Prosper Julius <prmavhunga@gmail.com>
2024-03-13 14:01:20 +0000

    changes the quiz app

--- src/feature/portfolio/quiz-capital/capital-city-opitions.tsx
+++ src/feature/portfolio/quiz-capital/capital-city-opitions.tsx
@@ -7,28 +7,33 @@ export const CapitalCityOptions = () => {
   const answer = useAppSelector((state) => state.capitalCityQuiz.answer);
   const options = useAppSelector((state) => state.capitalCityQuiz.options);
   const [disableSelection, setDisableSelection] = useState<boolean>(false);
-  const [userAnswer,setUserAnswer] = useState<string>("");
+  const [userAnswer, setUserAnswer] = useState<string>("");
 
   useEffect(() => {
     setDisableSelection(false);
+    setUserAnswer("");
   }, [answer?.capital]);
 
   const capitalCitiesOptions = options.map((country) => {
     return (
       <li
-      className={classNames({clicked:country.capital === userAnswer,btn:true})}
+        className={classNames({
+          clicked: country.capital === userAnswer,
+          btn: true,
+          correct:userAnswer.length && country.capital === answer?.capital,
+          wrong: userAnswer.length && country.capital === userAnswer && country.capital !== answer?.capital,
+        })}
         key={country.iso3}
         onClick={() => {
           if (!disableSelection) {
             setDisableSelection(true);
-            setUserAnswer(country.capital)
+            setUserAnswer(country.capital);
             country.capital === answer?.capital
               ? toast.success("Your answer is correct 😊")
               : toast.error(`The correct answer is ${answer?.capital} 😜`);
           }
         }}
       >
-       
         {country?.capital}
       </li>
     );
